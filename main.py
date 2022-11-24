9418d55113ae9d0b9ca3b58b41ce9661b7805e53
jfpichlme <66117575+jfpichlme@users.noreply.github.com>
2022-11-24 12:25:30 +0100

    Main control file

---
+++ main.py
@@ -0,0 +1,24 @@
+from Random_Nano_creator import Creator
+
+
+def main():
+
+    # First we initialize an Object that defines how many Rings we need to add
+    nano_graphenes_object = Creator(20, max_number_conn=4)
+
+    # Next, the graphene grid is set up from which we "cut out" the nanographenes randomly
+    nano_graphenes_object.set_up_grid()
+
+    # The nanographenes are created
+    nano_graphenes_object.create_graphene_structure()
+
+    # The nanographenes ar saved, plotted and an hydrogen atom is added to each unpaired electron
+    nano_graphenes_object.create_save_nanographenes()
+
+
+if __name__ == "__main__":
+    main()
+
+
+
+
